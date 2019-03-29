645b0533e202a661a422cf4a1f830265f18fa8d9
Chirag Jain <chiragcj96@gmail.com>
2019-03-28 23:07:13 -0400

    Added comments

--- read&writeThroughStreams.js
+++ read&writeThroughStreams.js
@@ -1,12 +1,12 @@
 var http = require('http');
 var fs = require('fs');
 
-var readStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
-var writeStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+var readStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');      //utf8 encoding for data to be read in defined way, readMe.txt is the file containing data to be read, Instead of __dirname, whole of the file path can be added.
+var writeStream = fs.createWriteStream(__dirname + '/writeMe.txt');     //writeStream is created using createWriteStream and saved via writeStream into writeMe.txt
 
-readStream.on('data', funtion(chunk){
-  console.log('new chunk received');
-  console.log(chunk);
-  writeStream.write(chunk);
+readStream.on('data', funtion(chunk){     //through the stream, data is sent in small chunks, getting collected into a Buffer. Buffer is then sent on stream.
+  console.log('new chunk received');      
+  console.log(chunk);     
+  writeStream.write(chunk);     //it writes the chunk of data into the writeMe.txt file through writeStream.
   
 });
