d5e81e68644f2431658b197dff4745475149b997
Chirag Jain <chiragcj96@gmail.com>
2019-03-28 22:54:35 -0400

    Added the code for Write stream

--- read&writeThroughStreams.js
+++ read&writeThroughStreams.js
@@ -2,9 +2,11 @@ var http = require('http');
 var fs = require('fs');
 
 var readStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+var writeStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
 readStream.on('data', funtion(chunk){
   console.log('new chunk received');
   console.log(chunk);
+  writeStream.write(chunk);
   
 });
